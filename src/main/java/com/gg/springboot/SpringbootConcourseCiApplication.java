f40dc49fffb30c878ef8764b861ba23d72322a8f
Gaurav <g.gupta@hcl.com>
2021-04-18 14:48:23 -0500

    Spring boot concourse ci project

---
+++ src/main/java/com/gg/springboot/SpringbootConcourseCiApplication.java
@@ -0,0 +1,32 @@
+package com.gg.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+
+@SpringBootApplication
+public class SpringbootConcourseCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootConcourseCiApplication.class, args);
+	}
+
+	 @Bean
+	    HealthIndicator health() {
+	        return () -> Health.status("I <3 Production!").build();
+	    }
+
+	    @Bean
+	    RouterFunction<ServerResponse> routes() {
+	        return route(GET("/api/hello"), request -> ok().syncBody("Hello, SpringBootKnolxDemoTest"));
+	    }
+	
+	
+}

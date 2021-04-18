059504d9ee64f00fc1828d79264b01a2d635b1c3
Gaurav <g.gupta@hcl.com>
2021-04-18 15:19:05 -0500

    Spring boot concourse ci project

--- concourse_ci/tasks/sonarscan.sh
+++ concourse_ci/tasks/sonarscan.sh
@@ -7,10 +7,10 @@ echo " .. Running build..."
 echo "git url is... "
 echo "${git_repo_url}"
 echo "$(pwd)"
-cd git-repo
+cd spring-boot-service
 dir=$(pwd -P)
 echo "The directory is "
 echo $dir
 
-./mvnw package --s settings.xml -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Dmaven.wagon.http.ssl.ignore.validity.dates=true -Dmaven.test.skip=true sonar:sonar -Dsonar.host.url=https://sonarqube.mypepsico.com -Dsonar.verbose=true -Dsonar.projectKey=OmniChannelEcommerceHub-OCH_EComm_Inventory_Service -Dsonar.projectName=OCH_EComm_Inventory_Service -Dsonar.exclusions=src/main/java/com/pepsico/**Factory.java,src/main/java/com/pepsico/**Constant.java,src/test/**,**.xml -Dsonar.java.binaries=target/classes
+./mvnw package --s settings.xml -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Dmaven.wagon.http.ssl.ignore.validity.dates=true -Dmaven.test.skip=true sonar:sonar -Dsonar.host.url=http://192.168.0.23:9000/ -Dsonar.verbose=true -Dsonar.projectKey=OmniChannelEcommerceHub-copy -Dsonar.projectName=OCH_EComm_Inventory_Service -Dsonar.exclusions=src/main/java/com/pepsico/**Factory.java,src/main/java/com/pepsico/**Constant.java,src/test/**,**.xml -Dsonar.java.binaries=target/classes
 
